c3aab1c65ff83534447d2de8ab83c169f9aefc8d
AsterVAIZ <ivanostapuk200511@gmail.com>
2026-06-10 16:14:20 +0300

    Відредаговано supported.py

--- commands/supported.py
+++ commands/supported.py
@@ -3,12 +3,14 @@ import commands.cd.cd
 import commands.echo.echo
 import commands.sort.sort
 import commands.cat.cat
+import commands.whoami.whoami
 
 COMMANDS = {
     "test": commands.test.test,
     "cd": commands.cd.cd,
     "echo": commands.echo.echo,
     "sort": commands.sort.sort,
-    "cat": commands.cat.cat
+    "cat": commands.cat.cat,
+    "whoami": commands.whoami.whoami
 }
 
